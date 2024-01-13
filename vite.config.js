17c26d09cc1e17fbb640ff01144de6e9b3a6ab93
Paul Jor <jorpaulvincent@gmail.com>
2024-01-13 20:58:39 +0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        port: 3000, //5173 default port
+        // host: true,
+        strictPort: true,
+        open: true,
+    },
+});
